a9028d317305235376617d7e437d1705dfb9e550
Danillo Leal <tmdanillomartins@gmail.com>
2024-06-11 17:00:24 -0300

    Camada Controller da aplicação em vaga.

--- src/main/java/com/danillo/vagas/controllers/CandidateController.java
+++ src/main/java/com/danillo/vagas/controllers/CandidateController.java
@@ -1,12 +1,10 @@
 package com.danillo.vagas.controllers;
 
 import com.danillo.vagas.models.candidate.Candidate;
-import com.danillo.vagas.models.candidate.exceptions.UserAlreadyExistsException;
-import com.danillo.vagas.repositories.CandidateRepository;
 import com.danillo.vagas.services.CandidateService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -31,6 +29,19 @@ public class CandidateController {
         try {
             var profile = this.candidateService.profileCandidate(Long.parseLong(idCandidate.toString()));
             return ResponseEntity.ok().body(profile);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<Object> applyJob(@RequestBody Long idJob, HttpServletRequest request) {
+        var idCandidate = request.getAttribute("candidate_id");
+        try {
+            var result = candidateService.applyJob(Long.parseLong(idCandidate.toString()), idJob);
+            return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
